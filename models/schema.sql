19a2e7a7e121e11eeb3a48ca0c288b7697c0b12c
Jordan Rabideau <jordanmrabideau@gmail.com>
2019-03-05 20:05:26 -0500

    added our schema

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,15 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS ghosted_by;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+CREATE DATABASE ghosted_by;
+USE ghosted_by;
+
+CREATE TABLE `ghostedby` (
+  `id` Int( 11 ) AUTO_INCREMENT NOT NULL,
+  `glassdoor_id` VARCHAR( 255) NOT NULL,
+  `company_name` VARCHAR( 255 ) NOT NULL,
+  `logo` VARCHAR( 255 ) NOT NULL,
+  `ghosted_count` VARCHAR( 255 ) NOT NULL,
+  `time_stamp` DATETIME NOT NULL,
+
+  PRIMARY KEY ( `id` ) 
+);
