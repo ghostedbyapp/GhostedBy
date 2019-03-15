9d7ef15f2938b95845c356d02e4783fcae14e07a
johndoeadams <johndoeadams@gmail.com>
2019-03-14 20:37:08 -0400

    removing unused handlebars

--- models/index.js
+++ models/index.js
@@ -1,11 +1,11 @@
 "use strict";
-var config = require(__dirname + "/../config/config.json")[env];
+
 var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || "production";
-
+var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
 if (config.use_env_variable) {
